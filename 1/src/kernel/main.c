c33caf5f9c53eea66eb727dcfd3be236afa11ece
bilel moudache <bilel.moudache@proton.me>
2025-10-15 15:09:40 +0200

    finish

--- 1/src/kernel/main.c
+++ 1/src/kernel/main.c
@@ -1,4 +1,10 @@
-int kmain()
+void kmain(void)
 {
-    return (0);
+    char *vga = (char *)0xB8000;  // adresse mémoire VGA texte
+    vga[0] = '4';                 // premier caractère
+    vga[1] = 0x07;                // couleur gris clair sur fond noir
+    vga[2] = '2';                 // second caractère
+    vga[3] = 0x07;                // même couleur
+    while (1)                     // boucle infinie pour garder l’écran affiché
+        ;
 }
