1bae41a26f917fee1eac715f7d935726a36cdd36
bilel moudache <bilel.moudache@proton.me>
2025-10-16 16:17:04 +0200

    define type for easiest readibility

---
+++ 1/src/kernel/multiboot2.h
@@ -0,0 +1,4 @@
+typedef unsigned char           multiboot_uint8_t;
+typedef unsigned short          multiboot_uint16_t;
+typedef unsigned int            multiboot_uint32_t;
+typedef unsigned long long      multiboot_uint64_t;
