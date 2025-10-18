2021f26e4c0fab88665d86d60c0c4ae6ed959ad1
SefirOutin <1stalias.stove861@passinbox.com>
2025-10-18 23:15:29 +0200

    vga text mode, cursor following

--- 1/src/kernel_main.c
+++ 1/src/kernel_main.c
@@ -1,26 +1,93 @@
 #include "kernel.h"
 
+#define VgaWidth 80
 
+volatile uint16_t *vga = (char *)0xB8000;
+
+typedef struct  terminal
+{
+    void    (*updateCursor) (struct terminal *term, int x, int y);
+    void    (*updatePos) (struct terminal *term, int x, int y);
+    int     (*write) (struct terminal *term, const char *str, uint32_t len);
+
+    int     xPos;
+    int     yPos;
+}                   tty;
+
+
+void enableCursor(tty *term, uint8_t cursor_start, uint8_t cursor_end)
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
+}
+
+void updateCursor(tty *term, int x, int y)
+{
+    term->xPos = x;
+    term->yPos = y;
+
+	uint16_t pos = y * VgaWidth + x;
+	
+    outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+}
+
+int write(tty *term, const char *str, uint32_t len)
+{
+    uint32_t j = 0;
+    uint32_t cursor = term->yPos * VgaWidth + term->xPos;
+
+    while(j < len)
+    {
+        vga[cursor + j] = str[j] | 0xa << 8;
+        j++;
+    }
+    cursor += j;
+    term->updateCursor(term, cursor % VgaWidth, cursor / VgaWidth);
+    return (j);
+}
+
+void    initTerm(tty *term)
+{
+    memset(term, 0, sizeof(term));
+    // term->enableCursor = enableCursor;
+    term->updateCursor = updateCursor;
+    term->write = write;
+}
 
 int kernel_main(uint32_t *s)
 {
-    t_multiboot_struct mb2Sruct;
-    // struct multiboot_tag *tag;
-	serialInit();
-    memset(&mb2Sruct, 0, sizeof(t_multiboot_struct));
-    // printf("\n%d\n", printf("oui %s", "non"));
-    // uint32_t structLenght = *s;
-    printf("struct len: %dbytes", sizeof(t_multiboot_struct));
-    // *((uint32_t *)0xA000) = 1;
-    // // uint8_t *tmp = (uint8_t *)s;
-    // for (tag = s + 8;
-    //     tag->type != MULTIBOOT_TAG_TYPE_END;
-    //     tag = (struct multiboot_tag *) ((uint8_t *) tag + ((tag->size + 7) & ~7)))
-    // {
+    tty term;
+    initTerm(&term);
+    printf("x: %d y: %d\n", term.xPos, term.yPos);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    term.write(&term, "oui", 3);
+    // term.updateCursor(&term, 1, 0);
+    // char *vga = (char *)0xB8000;  // adresse mémoire VGA texte
+    // vga[0] = '4';                 // premier caractère
+    // vga[1] = 0x07;                // couleur gris clair sur fond noir
+    // vga[2] = '2';                 // second caractère
+    // vga[3] = 0x07;                // même couleur
+    // outb(0x3D4, 0x0A);
+    // outb(0x3D5, 0x20);
+    // enable_cursor(14, 15);
+    // update_cursor(50, 1);
+    while (1);                     // boucle infinie pour garder l’écran affiché
         
-    // }
-    
-    
-    
-    while (1);
 }
--- 1/src/multiboot.s
+++ 1/src/multiboot.s
@@ -12,49 +12,22 @@ magic_fields:
 	dd 0x100000000 - (MAGIC_NUMBER + HEADER_LENGHT)		; checksum
 magic_fields_end:
 
-mbi_request_tag:
-	align 8
-	dw 1									; type multiboot2 information request
-	dw 0									; flags
-	dd mbi_request_tag_end - mbi_request_tag; size
-	dd 8, 0									; mdi_tag_types array
-mbi_request_tag_end:
-
-; address_tag:
+; flags_tag:
 ; 	align 8
-; 	dw 2									; type: address tag
+; 	dw 4									; type: console type
 ; 	dw 0									; flags
-; 	dd address_tag_end - address_tag		; size
-; 	dd magic_fields							; header address
-; 	dd _start								; load addr of code entry
-; 	dd 0									; end addr of code (here assumes its the whole iso)
-; 	dd bss_end								; end addr of bss section
-; address_tag_end:
+; 	dd 12									; size
+; 	dd 0b10									; EGA text support (console_flags)
 
-
-; entry_addr_tag:
+; framebuffer_tag:
 ; 	align 8
-; 	dw 3									; type: entry address tag
-; 	dw 0									; flags
-; 	dd entry_addr_tag_end - entry_addr_tag	; size
-; 	dd _start								; entry address into kernel
-; entry_addr_tag_end:
+; 	dw 5									; type: framebuffer
+; 	dw 0
+; 	dd 20									; size
+; 	dd 640
+; 	dd 480
+; 	dd 32
 
-flags_tag:
-	align 8
-	dw 4									; type: console type
-	dw 0									; flags
-	dd 12									; size
-	dd 0b11									; EGA text support (console_flags)
-
-framebuffer_tag:
-	align 8
-	dw 5									; type: framebuffer
-	dw 0
-	dd 20									; size
-	dd 800
-	dd 600
-	dd 32
 
 last_tag:
 	align 8
@@ -74,17 +47,12 @@ section .text
 	global _start
 
 _start:
-	; xor eax, eax
-	; xor ebx, ebx
-
 	mov esp, stack_top						; set up stack
 	and esp, 0xFFFFFFF0						; align 16bytes - mask to round down
 
 	cmp eax, 0x36d76289						; eax must have this magic number given by grub
 	jne	endk
 
-	; push eax								; push magic number (2nd arg x86 conv)
-	; mov byte [0xb8000], 69
 	push ebx								; push grub struct (1st arg x86 conv)
 	extern kernel_main
 	call kernel_main
