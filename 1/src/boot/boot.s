2bfa145356be125a450df70f39280fe0e739c940
bilel moudache <bilel.moudache@proton.me>
2025-10-18 19:42:11 +0200

    rename

--- 1/src/boot/boot.s
+++ 1/src/boot/boot.s
@@ -7,15 +7,15 @@ SECTION .multiboot_header
         dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start)) ;checksum 
 
         
-        framebuffer_tag_start:
-            dw  0x05    ;Type: framebuffer
-            dw  0x01    ;Optional tag
-            dd  framebuffer_tag_end - framebuffer_tag_start ;size
-            dd  0   ;Width - if 0 we let the bootloader decide
-            dd  0   ;Height - same as above
-            dd  0   ;Depth  - same as above
-        framebuffer_tag_end:        
-        
+        ;framebuffer_tag_start:
+        ;    dw  0x05    ;Type: framebuffer
+        ;    dw  0x01    ;Optional tag
+        ;    dd  framebuffer_tag_end - framebuffer_tag_start ;size
+        ;    dd  1024   ;Width - if 0 we let the bootloader decide
+        ;    dd  760  ;Height - same as above
+        ;    dd  32 ;Depth  - same as above
+        ;framebuffer_tag_end:        
+    
         ; required end tag
         dw 0    ;type
         dw 0    ;flag
@@ -26,11 +26,11 @@ SECTION .multiboot_header
 
 SECTION .text
 global _start
-extern kmain
+extern kkernel
 _start:
     mov esp,  stack_top
     push ebx
-    call kmain
+    call kkernel
 .loop:
     hlt
     jmp .loop
--- 1/src/kernel/kernel.c
+++ 1/src/kernel/kernel.c
@@ -1,5 +1,11 @@
-void kmain(void)
+#include "multiboot2.h"
+
+
+void kkernel(unsigned long magic, unsigned long addr)
 {
+
+struct multiboot_tag *tag;
+  unsigned size;
     char *vga = (char *)0xB8000;  // adresse mémoire VGA texte
     vga[0] = '4';                 // premier caractère
     vga[1] = 0x07;                // couleur gris clair sur fond noir
